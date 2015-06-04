73592106a0d2b0eaf5c74a857f5478b6811b880b
John Lindquist <johnlindquist@gmail.com>
2015-06-04 16:25:07 -0600

    moving shift

--- keyboard/planck/extended_keymaps/extended_keymap_john.c
+++ keyboard/planck/extended_keymaps/extended_keymap_john.c
@@ -2,21 +2,21 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = { /* Colemak */
-    {KC_TAB,            KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,           KC_J,               KC_L,           KC_U,               KC_Y,               KC_SCLN,        KC_ESC},
-    {FUNC(2),           KC_A,           KC_R,           KC_S,           KC_T,           KC_D,           KC_H,               KC_N,           KC_E,               KC_I,               KC_O,           KC_ENT},
+    {KC_TAB,            KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,           KC_J,               KC_L,           KC_U,               KC_Y,               KC_SCLN,        KC_ENT},
+    {FUNC(2),           KC_A,           KC_R,           KC_S,           KC_T,           KC_D,           KC_H,               KC_N,           KC_E,               KC_I,               KC_O,           KC_QUOT},
     {KC_BSPC,           KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_K,               KC_M,           KC_COMM,            KC_DOT,             KC_SLSH,        KC_DELETE},
-    {LGUI(KC_Z),        KC_LCTL,        KC_LALT,        FUNC(10),       KC_LALT,        FUNC(20),       FUNC(1),            KC_LGUI,        KC_NO,              KC_NO,              KC_RALT,        KC_RCTL}
+    {LGUI(KC_Z),        KC_LCTL,        KC_LALT,        FUNC(10),       KC_LSFT,        KC_SPC,         FUNC(1),            KC_LGUI,        KC_NO,              KC_RALT,            KC_RCTL,        KC_ESC}
 },   
 [1] = { /* NUMBERS AND SYMBOLS */         
     {KC_GRV,            KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,               KC_7,           KC_8,               KC_9,               KC_0,           KC_BSLASH},
     {KC_TRNS,           KC_LBRC,        KC_RBRC,        KC_QUOT,        S(KC_QUOT),     S(KC_EQL),      KC_MINS,            S(KC_LBRC),     S(KC_RBRC),         S(KC_9),            S(KC_0),        KC_NO},
-    {S(KC_GRV),         S(KC_1),        S(KC_2),        S(KC_3),        S(KC_4),        S(KC_5),        S(KC_6),            S(KC_7),        S(KC_8),            S(KC_9),            S(KC_0),        KC_NO},
+    {S(KC_GRV),         S(KC_1),        S(KC_2),        S(KC_3),        S(KC_4),        S(KC_5),        S(KC_6),            S(KC_7),        S(KC_8),            S(KC_9),            S(KC_0),        KC_TRNS},
     {KC_TRNS,           KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,        KC_TRNS,            KC_TRNS,            KC_TRNS,        KC_TRNS}
 },      
-[2] = { /* MOVEMENT AND FN*/     
+[2] = { /* HJKL MOVEMENT AND FN*/     
     {KC_F12,            KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,              KC_F7,          KC_F8,              KC_F9,              KC_F10,         KC_F11},
-    {KC_TRNS,           LALT(KC_SPC),   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_LEFT,            KC_DOWN,        KC_UP,              KC_RGHT,            KC_NO,          KC_NO},
-    {KC_NO,             KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_WH_R,            KC_WH_U,        KC_WH_D,            KC_WH_L,            KC_NO,          KC_NO},
+    {KC_TRNS,           LALT(KC_SPC),   KC_LCTL,        KC_LSFT,        KC_LALT,        KC_ESC,         KC_LEFT,            KC_DOWN,        KC_UP,              KC_RGHT,            KC_NO,          KC_NO},
+    {KC_TRNS,           KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_BSPC,            KC_WH_U,        KC_WH_D,            KC_DELETE,          KC_NO,          KC_TRNS},
     {KC_TRNS,           KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,        KC_TRNS,            RESET,              KC_TRNS,        KC_TRNS}
 },
 };
