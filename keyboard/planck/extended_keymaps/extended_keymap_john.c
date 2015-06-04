f3140f4101ab76ec8b8a6a492d8bbce5c56bb83a
John Lindquist <johnlindquist@gmail.com>
2015-06-04 16:45:51 -0600

    moving shift again

--- keyboard/planck/extended_keymaps/extended_keymap_john.c
+++ keyboard/planck/extended_keymaps/extended_keymap_john.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_TAB,            KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,           KC_J,               KC_L,           KC_U,               KC_Y,               KC_SCLN,        KC_ENT},
     {FUNC(2),           KC_A,           KC_R,           KC_S,           KC_T,           KC_D,           KC_H,               KC_N,           KC_E,               KC_I,               KC_O,           KC_QUOT},
     {KC_BSPC,           KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_K,               KC_M,           KC_COMM,            KC_DOT,             KC_SLSH,        KC_DELETE},
-    {LGUI(KC_Z),        KC_LCTL,        KC_LALT,        FUNC(10),       KC_LSFT,        KC_SPC,         FUNC(1),            KC_LGUI,        KC_NO,              KC_RALT,            KC_RCTL,        KC_ESC}
+    {LGUI(KC_Z),        KC_LCTL,        KC_LALT,        FUNC(10),       KC_LGUI,        KC_SPC,         KC_LSFT,            FUNC(1),        KC_NO,              KC_RALT,            KC_RCTL,        KC_MINS}
 },   
 [1] = { /* NUMBERS AND SYMBOLS */         
     {KC_GRV,            KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,               KC_7,           KC_8,               KC_9,               KC_0,           KC_BSLASH},
